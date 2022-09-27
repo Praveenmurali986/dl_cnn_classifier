5cd8d3eaec838da5d8d8412534dfba3706cd22ba
Praveenmurali986 <praveenmurali986@gmail.com>
2022-09-27 21:03:56 +0530

    common util updated

--- src/deepClassifier/utils/common.py
+++ src/deepClassifier/utils/common.py
@@ -35,4 +35,65 @@ def read_yaml(path_to_yaml:Path)->ConfigBox:
     except Exception as e :
         raise e 
 
+@ensure_annotations
+def create_directories(path_to_directories:list,verbose=True):
+    '''create list of directories
+
+    Args:
+        path_to_directories(list): list of path of directories
+        ignore_log (bool,optional): ignore if multiple dirs is to be created.Defaults to False.
+    '''
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f'created directory at: {path}')
+
+@ensure_annotations
+def save_json(path:Path,data:dict):
+    '''
+    save json data
+
+    Args:
+        path(Path) : path to json file
+        data(dict) : data to be saved in json file
+    '''
+    with open(path,'w') as f:
+        json.dump(data,f,indent=4)
+
+    logger.info(f'json file saved at: {path}')
+
+@ensure_annotations
+def load_json(path:Path)->ConfigBox:
+    '''
+    load json files data
+
+    Args: 
+        path(Path): path to json file
+
+    returns: 
+        ConfigBox : data as class attributes insted of dict 
+    '''
+
+    with open(path) as f:
+        content=json.load(f)
+    logger.info(f'json file loaded successfully from: {path}')
+    return ConfigBox(content)
+
+@ensure_annotations
+def get_size(path:Path)->str:
+    '''
+    get size in KB
+
+    Args: 
+        path(Path): path of the file
+
+    Returns:
+        str: size in KB
+    '''
+    size_in_kb= round(os.path.getsize(path)/1024)
+    return f'~{size_in_kb}KB'
+
+
+
+
 
